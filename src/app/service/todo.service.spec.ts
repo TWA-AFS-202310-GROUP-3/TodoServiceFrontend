30381248dfe0071ab69f27d824f4673b1e9d81fc
Kailin Liu <KLiu13@slb.com>
2023-11-10 14:21:11 +0800

    test: add get all test case in todo service

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -8,9 +8,27 @@ describe('TodoService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
+    service.items = [
+      {
+        id: 1,
+        title: 'xianke',
+        description: 'csgo',
+        isDone: false
+      }
+    ]
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get all items when call getAll', () => {
+    const item = service.getAll()
+    expect(item).toEqual([{
+        id: 1,
+        title: 'xianke',
+        description: 'csgo',
+        isDone: false
+    }]);
   });
 });
