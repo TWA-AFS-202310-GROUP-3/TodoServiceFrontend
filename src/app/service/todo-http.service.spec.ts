0247a92f8a4505407a48e57b12b7ca3e743baac4
YChen110 <Ychen110@slb.com>
2023-11-11 02:18:36 +0800

    test: add test for getOne and delete http service

--- src/app/service/todo-http.service.spec.ts
+++ src/app/service/todo-http.service.spec.ts
@@ -11,16 +11,23 @@ describe('TodoHttpService',
   () => {
     let service: TodoHttpService;
     let httpClientSpy: jasmine.SpyObj<HttpClient>;
-    const fakeItem: ToDoItem = {
+    const fakeItem1: ToDoItem = {
       'id': 0,
       'title': 'buy food',
       'description': 'buy lunch',
       'isDone': false
     };
 
+    const fakeItem2: ToDoItem = {
+      'id': 1,
+      'title': 'buy drink',
+      'description': 'buy some drink',
+      'isDone': false
+    };
+
     beforeEach(() => {
       //TestBed.configureTestingModule({});
-      httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'delete']);
+      httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'delete', 'put']);
       //service = TestBed.inject(TodoHttpService);
       service = new TodoHttpService(httpClientSpy);
     });
@@ -31,20 +38,33 @@ describe('TodoHttpService',
 
     it('should get all todo items when call getAll', () => {
       httpClientSpy.get.and.returnValue(asyncData(
-        [fakeItem]
+        [fakeItem1, fakeItem2]
       ));
       service.getAll().subscribe(data =>
-        expect(data.length).toEqual(1));
+        expect(data.length).toEqual(2));
+    });
+
+    it('should get one todo items when call getItemById', () => {
+      httpClientSpy.get.and.returnValue(asyncData(
+        [fakeItem1]
+      ));
+      service.get(0).subscribe(data => {
+        expect(data.id).toEqual(fakeItem1.id);
+        expect(data.title).toEqual(fakeItem1.title);
+        expect(data.description).toEqual(fakeItem1.description);
+        expect(data.isDone).toEqual(fakeItem1.isDone);
+      });
     });
 
     it('should create one todo items when call create', () => {
-      httpClientSpy.post.and.returnValue(asyncData([fakeItem]
+      httpClientSpy.post.and.returnValue(asyncData([fakeItem1]
       ));
-      service.create("buy food", "buy lunch").subscribe(data =>{
-        expect(data.id).toEqual(fakeItem.id);
-        expect(data.title).toEqual(fakeItem.title);
-        expect(data.description).toEqual(fakeItem.description);
-        expect(data.isDone).toEqual(fakeItem.isDone);
-      })
+      service.create('buy food', 'buy lunch').subscribe();
+      expect(httpClientSpy.post).toHaveBeenCalledTimes(1);
+    });
+
+    it('should delete one todo items when call delete', () => {
+      service.delete(0).subscribe();
+      expect(httpClientSpy.delete).toHaveBeenCalledTimes(1);
     });
   });
