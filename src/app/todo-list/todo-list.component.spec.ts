fdfe96521b28f3f3ac1fc8e021a8cb2b9da9d52b
Kailin Liu <KLiu13@slb.com>
2023-11-10 15:20:07 +0800

    feat: add gotodetail page in todo list page

--- src/app/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoListComponent } from './todo-list.component';
+import { CreateTodoComponent } from '../create-todo/create-todo.component';
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
@@ -8,7 +9,7 @@ describe('TodoListComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TodoListComponent]
+      declarations: [TodoListComponent, CreateTodoComponent]
     });
     fixture = TestBed.createComponent(TodoListComponent);
     component = fixture.componentInstance;
