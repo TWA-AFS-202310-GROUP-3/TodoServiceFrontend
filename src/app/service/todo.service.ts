c8320521425f7aaf816e6f5bb8cc43da20e820b0
Kailin Liu <KLiu13@slb.com>
2023-11-10 13:33:15 +0800

    feat: add todo service

---
+++ src/app/service/todo.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { ToDoItem } from 'src/model/ToDoItem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  items: ToDoItem[] = [
+    {
+      id: 1,
+      title: 'xianke',
+      description: 'csgo',
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: 'carzy',
+      description: 'csgo',
+      isDone: false,
+    },
+    {
+      id: 3,
+      title: 'genshuo',
+      description: 'csgo',
+      isDone: false,
+    },
+  ];
+
+  constructor() { }
+
+  getAll(){
+    return this.items
+  }
+}
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ToDoItem } from 'src/model/ToDoItem';
+import { TodoService } from '../service/todo.service';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,24 +8,11 @@ import { ToDoItem } from 'src/model/ToDoItem';
   styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
-  items: ToDoItem[] = [
-    {
-      id: 1,
-      title: 'xianke',
-      description: 'csgo',
-      isDone: false,
-    },
-    {
-      id: 2,
-      title: 'carzy',
-      description: 'csgo',
-      isDone: false,
-    },
-    {
-      id: 3,
-      title: 'genshuo',
-      description: 'csgo',
-      isDone: false,
-    },
-  ];
+  items: ToDoItem[] = []
+
+  constructor(private todoService: TodoService){}
+
+  ngOnInit(){
+    this.items = this.todoService.getAll()
+  }
 }
