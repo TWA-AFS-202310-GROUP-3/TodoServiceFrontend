7f06f36c9d733feeb67257c108b0580f8c4a1842
Pengyu <PChen10@slb.com>
2023-11-10 16:12:11 +0800

    feat: add httpService client to connect backend

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ToDoItem } from 'src/model/ToDoItem';
 import { TodoService } from '../service/todo.service';
 import { Router } from '@angular/router';
+import { TodoHttpService } from '../service/todo-http.service';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -10,10 +11,16 @@ import { Router } from '@angular/router';
 export class TodoListComponent {
   items: ToDoItem[] = [];
 
-  constructor(private todoService: TodoService, private router: Router) {}
+  constructor(
+    private todoService: TodoService,
+    private router: Router,
+    private todoHttpService: TodoHttpService
+  ) {}
 
   ngOnInit() {
-    this.items = this.todoService.getAll();
+    this.todoHttpService.getAll().subscribe((todoItems) => {
+      this.items = todoItems;
+    });
   }
 
   onMarkDone(id: number) {
