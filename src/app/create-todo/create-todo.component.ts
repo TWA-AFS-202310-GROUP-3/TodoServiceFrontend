b8c1fa205be3d896f991fbbdf7957e5db639aa1a
Kailin Liu <KLiu13@slb.com>
2023-11-10 16:52:13 +0800

    feat: add refresh when created

--- src/app/create-todo/create-todo.component.ts
+++ src/app/create-todo/create-todo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { TodoService } from '../service/todo.service';
 import { TodoHttpService } from '../service/todo-http.service';
@@ -9,6 +9,9 @@ import { TodoHttpService } from '../service/todo-http.service';
   styleUrls: ['./create-todo.component.css']
 })
 export class CreateTodoComponent {
+
+  @Output() created = new EventEmitter()
+
   constructor(private formBuilder: FormBuilder,
      private todoService: TodoService,
      private todoHttpService: TodoHttpService){}
@@ -26,6 +29,7 @@ export class CreateTodoComponent {
       this.todoHttpService.create(formValues.title,
         formValues.description).subscribe(() => {
           this.todoForm.reset()
+          this.created.emit()
         })
     }
   }
