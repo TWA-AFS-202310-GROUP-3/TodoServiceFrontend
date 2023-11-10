170b9b9af0fe68afaf16e42394494e73d89f3877
YChen110 <Ychen110@slb.com>
2023-11-10 15:12:45 +0800

    feat: add todo detail page

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -28,12 +28,17 @@ export class TodoService {
     const item = this.todoItems.find(item => item.id === id);
     if (item) item.isDone = true;
   }
-  createItem(title: string, description: string):void {
+  public createItem(title: string, description: string):void {
     this.todoItems.push({
       id:this.todoItems.length+1,
       title: title,
       description: description,
       isDone: false
     })
+  }
+  public getItemById(id: number): ToDoItem | undefined {
+    const item = this.todoItems.find(item => item.id === id);
+    if (item) return item;
+    return undefined;
   }
 }
--- src/app/todo-detail/todo-detail.component.ts
+++ src/app/todo-detail/todo-detail.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToDoItem } from '../../model/ToDoItem';
+import { TodoService } from '../service/todo.service';
 
 @Component({
   selector: 'app-todo-detail',
   templateUrl: './todo-detail.component.html',
   styleUrls: ['./todo-detail.component.css']
 })
-export class TodoDetailComponent {
+export class TodoDetailComponent implements OnInit{
+  item: ToDoItem | undefined = undefined;
+  constructor(private activatedRouter: ActivatedRoute, private todoService: TodoService) {
+  }
+
+  ngOnInit(): void {
+    const id = this.activatedRouter.snapshot.paramMap.get("detailId");
+    console.log(id);
+    this.item = this.todoService.getItemById(Number(id));
+  }
+
 
 }
