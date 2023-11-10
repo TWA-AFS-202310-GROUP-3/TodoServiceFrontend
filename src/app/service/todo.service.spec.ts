25841e21f820835fa64ef6fff7c7a3b196fc36d9
Pengyu <PChen10@slb.com>
2023-11-10 14:21:21 +0800

    test: add a service test case when call getAll

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -8,9 +8,19 @@ describe('TodoService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
+    service.items = [
+      { id: 1, title: 'hi', description: 'sayhi', isDone: false },
+    ];
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get all items when call getAll', () => {
+    const items = service.getAll();
+    expect(items).toEqual([
+      { id: 1, title: 'hi', description: 'sayhi', isDone: false },
+    ]);
   });
 });
