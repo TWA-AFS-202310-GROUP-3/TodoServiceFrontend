445bdad9679d3578cfec28150dc5a58153d43df2
Shuhan Jin <sjin9@slb.com>
2023-11-10 14:26:00 +0800

    test: add test for create and done

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -11,11 +11,11 @@ describe('TodoService', () => {
     service.items = [
       //准备测试数据
       {
-        id: 2,
+        id: 1,
         title: 'buy bread',
         description: 'buy some bread',
         isDone: false,
-      }
+      },
     ];
   });
 
@@ -23,16 +23,48 @@ describe('TodoService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should get all items when call getAll', ()=>{
-    const items = service.getAll()
+  it('should get all items when call getAll', () => {
+    const items = service.getAll();
 
     expect(items).toEqual([
       {
-        id: 2,
+        id: 1,
         title: 'buy bread',
         description: 'buy some bread',
         isDone: false,
-      }
+      },
     ]);
-  })
+  });
+
+  it('should create new item when call create', () => {
+    service.create('new item', 'new item description');
+
+    expect(service.items).toEqual([
+      {
+        id: 1,
+        title: 'buy bread',
+        description: 'buy some bread',
+        isDone: false,
+      },
+      {
+        id: 2,
+        title: 'new item',
+        description: 'new item description',
+        isDone: false,
+      },
+    ]);
+  });
+
+  it('should mark item as done when call markDone', () => {
+    service.markDone(1);
+
+    expect(service.items).toEqual([
+      {
+        id: 1,
+        title: 'buy bread',
+        description: 'buy some bread',
+        isDone: true,
+      },
+    ]);
+  });
 });
