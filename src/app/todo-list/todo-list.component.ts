ae064f51c3c90690c8b2b31f067be265d806a564
YChen110 <Ychen110@slb.com>
2023-11-10 13:10:52 +0800

    feat: add basic todoList table and onComplete

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ToDoItem } from '../../model/ToDoItem';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+  public todoItems: ToDoItem[] = [
+    {
+      id: 1,
+      title: 'buy milk',
+      description: 'buy some milk',
+      isDone: false
+    },
+    {
+      id: 2,
+      title: 'buy bubble tea',
+      description: 'buy some bubble tea',
+      isDone: false
+    }
+  ];
+  constructor(private fb: FormBuilder) {
+  }
+
+  onComplete(id:number):void {
+    let item = this.todoItems.find(item => item.id === id);
+    if (item) item.isDone = true;
+  }
+}
