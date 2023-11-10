79ebc3c7fb001ce5cd2d9c1e8ee3b94ffc20ef04
Wang Jing <jwang315@slb.com>
2023-11-10 14:26:11 +0800

    test: add test for getAll() and create()

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -5,12 +5,52 @@ import { TodoService } from './todo.service';
 describe('TodoService', () => {
   let service: TodoService;
 
+  /*beforeEach作用：在每次测试之前都跑一遍这些代码。 */
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
+    service.items = [
+      {
+        id: 1,
+        title: 'buy milk',
+        description: 'buy some milk',
+        isDone: false,
+      },
+    ];
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get all items when call getAll()', () => {
+    const items = service.getAll();
+    expect(items).toEqual([
+      {
+        id: 1,
+        title: 'buy milk',
+        description: 'buy some milk',
+        isDone: false,
+      },
+    ]);
+  });
+
+  it('should create new item when call create', () => {
+    service.create('buy bread', 'buy some bread');
+
+    expect(service.items).toEqual([
+      {
+        id: 1,
+        title: 'buy milk',
+        description: 'buy some milk',
+        isDone: false,
+      },
+      {
+        id: 2,
+        title: 'buy bread',
+        description: 'buy some bread',
+        isDone: false,
+      },
+    ]);
   });
 });
