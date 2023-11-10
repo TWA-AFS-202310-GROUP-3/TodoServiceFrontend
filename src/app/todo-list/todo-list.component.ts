504982085a8f350ced768cbd73709a750dc8c7c3
Shuhan Jin <sjin9@slb.com>
2023-11-10 15:19:52 +0800

    feat: add onGoToDetail to direct list to detail page

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ToDoItem } from 'src/model/ToDoItem';
 import { TodoService } from '../service/todo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo-list',
@@ -13,7 +14,8 @@ export class TodoListComponent {
   constructor(
     //constructor将需要用的属性加进来
     //注入todoService
-    private todoService: TodoService
+    private todoService: TodoService,
+    private router:Router
   ) {}
   ngOnInit() {
     //在组建load（初始化）时调用
@@ -23,5 +25,10 @@ export class TodoListComponent {
   onMarkDone(id: number) {
     //on开头因为是响应事件
     this.todoService.markDone(id);
+  }
+
+  onGoToDetail(id:number){
+    this.router.navigateByUrl(`/detail/${id}`) //use router to direct url
+    //need to use `` instead of '',otherwise will recognise id just as id
   }
 }
