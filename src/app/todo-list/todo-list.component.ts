f291e5d0a0a4fd472a05ba31d49cd88081af6082
Wang Jing <jwang315@slb.com>
2023-11-10 14:03:57 +0800

    feat: add markDone logic in todo list component

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -5,13 +5,17 @@ import { TodoService } from '../service/todo.service';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
-  items: ToDoItem[] =[]
-  constructor(private todoService: TodoService){}
-  ngOnInit(){   
+  items: ToDoItem[] = [];
+  constructor(private todoService: TodoService) {}
+  ngOnInit() {
     /*在组件初始化时就会调用ngOnInit() */
-    this.items = this.todoService.getAll()
+    this.items = this.todoService.getAll();
+  }
+
+  onMarkDown(id: number) {
+    this.todoService.markDone(id);
   }
 }
