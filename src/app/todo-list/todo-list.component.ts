6e123b55267ece6552096f39aeacda0b12e31f7a
Shuhan Jin <sjin9@slb.com>
2023-11-10 11:30:01 +0800

    feat: add todolist component

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ToDoItem } from 'src/model/ToDoItem';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+  items:ToDoItem[] =[ //prepare two data
+    {
+      id:1,
+      title:"buy milk",
+      description:"buy some milk",
+      isDone:false
+    },
+    {
+      id:2,
+      title:"buy bread",
+      description:"buy some bread",
+      isDone:false
+    }
+  
+  ]
+
+}
