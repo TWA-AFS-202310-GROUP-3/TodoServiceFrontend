354e4e4e32906c34814c393c3803f233103f3666
Pengyu <PChen10@slb.com>
2023-11-10 14:31:00 +0800

    test: add test for todo service

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -23,4 +23,24 @@ describe('TodoService', () => {
       { id: 1, title: 'hi', description: 'sayhi', isDone: false },
     ]);
   });
+
+  it('should create item when call create', () => {
+    service.create('test', 'test');
+    expect(service.items).toEqual([
+      { id: 1, title: 'hi', description: 'sayhi', isDone: false },
+      { id: 2, title: 'test', description: 'test', isDone: false },
+    ]);
+  });
+
+  it('should markDone current item when call markDone', () => {
+    service.markDone(1);
+    expect(service.items).toEqual([
+      {
+        id: 1,
+        title: 'hi',
+        description: 'sayhi',
+        isDone: true,
+      },
+    ]);
+  });
 });
