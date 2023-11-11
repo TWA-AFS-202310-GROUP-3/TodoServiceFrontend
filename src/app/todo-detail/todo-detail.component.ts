62cd6bdee95a1666de100b505863c6846a818409
Kailin Liu <KLiu13@slb.com>
2023-11-12 04:11:23 +0800

    feat: add change title and description function

--- src/app/todo-detail/todo-detail.component.ts
+++ src/app/todo-detail/todo-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ToDoItem } from 'src/model/ToDoItem';
 import { TodoService } from '../service/todo.service';
 import { TodoHttpService } from '../service/todo-http.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-detail',
@@ -12,16 +13,32 @@ import { TodoHttpService } from '../service/todo-http.service';
 export class TodoDetailComponent {
 
   item: ToDoItem | undefined
+  
 
   constructor(private activatedRoute: ActivatedRoute,
     private todoService: TodoService, 
-    private todoHttpService: TodoHttpService){}
+    private todoHttpService: TodoHttpService, 
+    private formBuilder: FormBuilder){}
 
+  todoForm = this.formBuilder.group({
+    title: '',
+    description: ''
+  })
+  
   ngOnInit(){
     const id = this.activatedRoute.snapshot.paramMap.get('detailId')
     //this.item = this.todoService.getItemById(Number(id))
     this.todoHttpService.getItemById(Number(id)).subscribe((item) => {
       this.item = item
     })
+  }
+
+  onUpdate() {
+    const formValues = this.todoForm.value
+    if (this.item && formValues.title && formValues.description) {
+      this.item.title = formValues.title
+      this.item.description = formValues.description
+      this.todoHttpService.updateItem(this.item).subscribe()
+    }
   }
 }
